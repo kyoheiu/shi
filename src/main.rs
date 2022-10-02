f9f0e50ca905f190f31195c8f3ecc4ba58081610
Kyohei Uto <kyoheiu@outlook.com>
2022-10-03 05:56:56 +0900

    MVP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+use std::fmt::Write as _;
+
+const DEFAULT_SIZE: usize = 50;
+
 fn main() {
-    println!("Hello, world!");
+    let connection = sqlite::open("test").unwrap();
+    connection
+        .execute(
+            "
+        CREATE TABLE IF NOT EXISTS history (
+            id INTEGER PRIMARY KEY,
+            time DATETIME NOT NULL,
+            command TEXT NOT NULL
+        );
+        ",
+        )
+        .unwrap();
+
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() == 1 {
+        connection
+            .iterate("SELECT * FROM history", |pairs| {
+                let mut count: usize = 0;
+                let mut result = String::new();
+                for &(column, value) in pairs.iter() {
+                    if column == "id" {
+                        result.push_str(value.unwrap());
+                    } else {
+                        let _ = write!(result, " {}", value.unwrap());
+                    }
+                    count += 1;
+                }
+                println!("{}", result);
+                count != DEFAULT_SIZE
+            })
+            .unwrap();
+    } else if args[1] == "insert" && args.len() > 2 {
+        if args[2].starts_with(' ') {
+            return;
+        } else {
+            let command = args[2..args.len()].join(" ");
+            connection
+                .execute(format!(
+                    "
+                    INSERT INTO history (time, command) VALUES (datetime('now', 'localtime'), '{}');
+                    ",
+                    command
+                ))
+                .unwrap();
+        }
+    } else if args[1] == "rm" {
+        connection
+            .execute(
+                "
+        DROP TABLE IF EXISTS history;
+        ",
+            )
+            .unwrap();
+    }
 }
